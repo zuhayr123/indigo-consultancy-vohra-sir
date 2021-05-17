89e55ab0972fb362efb8e4a72171e04a4f9daa16
root <you@example.com>
2021-05-18 00:44:47 +0530

    the bar is finally ready

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-
 const linksa = document.querySelectorAll(".nav-links li a");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fades");
-    });
+hamburger.addEventListener('click', () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fades");
+  });
 
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
 });
 
-$(document).ready(function() {
-    $(window).on("scroll", function() {
-      if ($(window).scrollTop() >= 20) {
-        $(".navbar").removeClass("compressed");
-        linksa.forEach(link => {
-          link.classList.add("color_change");
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= 20) {
+      $(".navbar").removeClass("compressed");
+      linksa.forEach(link => {
+        link.classList.add("color_change");
+        $(".logo").addClass("color_change_l");
       });
-      } else {
-        $(".navbar").addClass("compressed");
-        linksa.forEach(link => {
-          link.classList.remove("color_change");
+    } else {
+      $(".navbar").addClass("compressed");
+      linksa.forEach(link => {
+        link.classList.remove("color_change");
+        $(".logo").removeClass("color_change_l");
       });
-      }
-    });
+    }
   });
+});
