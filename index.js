0b375268514ca7e7a49e4540fe37c99a92c6c2f0
root <you@example.com>
2021-05-17 23:50:18 +0530

    the website is ready for some serious development

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+const linksa = document.querySelectorAll(".nav-links li a");
+
 hamburger.addEventListener('click', ()=>{
    //Animate Links
     navLinks.classList.toggle("open");
@@ -17,9 +19,14 @@ $(document).ready(function() {
     $(window).on("scroll", function() {
       if ($(window).scrollTop() >= 20) {
         $(".navbar").removeClass("compressed");
+        linksa.forEach(link => {
+          link.classList.add("color_change");
+      });
       } else {
         $(".navbar").addClass("compressed");
+        linksa.forEach(link => {
+          link.classList.remove("color_change");
+      });
       }
     });
   });
-  
