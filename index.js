7545c60b44e8e3df7d547e70efe4bef2fca2fb6a
root <you@example.com>
2021-05-17 15:11:49 +0530

    the bootstrap functions are now working fine

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ hamburger.addEventListener('click', ()=>{
    //Animate Links
     navLinks.classList.toggle("open");
     links.forEach(link => {
-        link.classList.toggle("fade");
+        link.classList.toggle("fades");
     });
 
     //Hamburger Animation
